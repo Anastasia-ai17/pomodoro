5e53a712c92dc25077f12e82a91dae2c7d242b12
Ksenya <rioooooooooooo@yandex.ru>
2026-03-07 12:27:08 +0300

    fix

--- pomodoro_project/settings.py
+++ pomodoro_project/settings.py
@@ -115,10 +115,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+
+
+# DRF settings
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ]
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 AUTH_USER_MODEL = 'users.User'
