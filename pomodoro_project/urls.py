6f0fd4d70396c1e884603bbec569aad3fc6f8dff
Nastya <nastyabineeva@gmail.com>
2026-03-22 21:37:52 +0300

    change urls %{}

--- pomodoro_project/urls.py
+++ pomodoro_project/urls.py
@@ -15,9 +15,9 @@ from users import views as users_views  # imported in case you need direct refer
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('regist', pomodoro_views.regist, name='regist'),
+    path('regist/', pomodoro_views.regist, name='regist'),
     path('', pomodoro_views.index, name='index'),
-    path('auth', pomodoro_views.auth, name='auth'),
-    path('person', pomodoro_views.person, name='person'),
+    path('auth/', pomodoro_views.auth, name='auth'),
+    path('person/', pomodoro_views.person, name='person'),
     path('api/', include('users.urls')),
 ]
