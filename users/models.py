17838b09fc39211ad91dcd34ac76fec4fa31d357
Nastya <nastyabineeva@gmail.com>
2026-04-02 15:04:42 +0300

    fix person page with api

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     coins = models.IntegerField(default=0)
-    
+    avatar = models.CharField(max_length=100, default='fa-dog')
     THEME_CHOICES = [
         ('light', 'Light'),
         ('dark', 'Dark'),
